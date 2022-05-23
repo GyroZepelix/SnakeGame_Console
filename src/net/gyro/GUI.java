cdd39368c0c00f46b104d3bfe41ca2a8934c2f6e
Gyro <gamingskullgamer@gmail.com>
2022-05-23 15:33:41 +0200

    .

---
+++ src/net/gyro/GUI.java
@@ -0,0 +1,32 @@
+package net.gyro;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame{
+    GUI() {
+        setResizable(false);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("SuS machine");
+
+        JLabel _label = new JLabel();
+        _label.setBounds(20,);
+        add(_label);
+
+        JButton _button = new JButton("? SUS ?", new ImageIcon("sus.png"));
+        _button.setBounds(20,200,450,50);
+        add(_button);
+
+        _button.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                _button.setText("Amogus");
+            }
+        });
+
+        setSize(500,500);
+        setLayout(null);
+        setVisible(true);
+    }
+}
--- src/net/gyro/Main.java
+++ src/net/gyro/Main.java
@@ -7,18 +7,7 @@ import java.io.Console;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(300,300,10,30));
-        panel.setLayout(new GridLayout(0,1));
-
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Sex Machine");
-        frame.pack();
-        frame.setVisible(true);
-
-
+        new GUI();
 
 
         /*Grid test = new Grid(25,25);
